6eee9b3eda2c715d7a3199862c7cfc9eda9da36e
Simon Grondin <simon.grondin@outlook.com>
2021-07-09 14:30:54 -0500

    1.2.0

--- src/expert.ml
+++ src/expert.ml
@@ -7,14 +7,6 @@ type 'a backpressure = {
   stream: 'a Lwt_stream.t;
 }
 
-(**
-  [SZXX.Expert.backpressure ic]
-
-  [controlled_ic]: Pass this channel to [SZXX.ZIP.stream_files].
-  [push]: Calling this function will populate the [stream], which will indirectly slow down [controlled_ic]. If you're using [SZXX.Zip.Action.Chunk], this is the function you must pass to it.
-  [finalize]: Await this promise along with the promise returned by [SZXX.ZIP.stream_files].
-  [stream]: Pull from this stream. Pulling faster will also make [controlled_ic] go faster.
-*)
 let backpressure input_channel =
   let mutex = Lwt_mutex.create () in
   let queue = Queue.create () in
